44b512849da19bbcc8177a0a0c65d7f2c396b17a
Hiroshi Tashiro <43472251+tys-hiroshi@users.noreply.github.com>
2021-03-12 20:12:32 +0900

    Update logger.py

--- src/log_utils/logger.py
+++ src/log_utils/logger.py
@@ -1,20 +1,21 @@
 import logging.config
 
-# def __init__(self, base_path: str, config_file_name: str):
-#     self.logging_config_file_path = os.path.join(base_path, config_file_name)
-
 class Logger():
     def __init__(self, logging_config_file_path: str):
         logging.config.fileConfig(logging_config_file_path)
-        logging.basicConfig(filename='logfile/logger.log', level=logging.DEBUG)
         self.logger = logging.getLogger()
 
-    def info(self, message, console=False):
+    def exception(self, message: str, exception: Exception, console: bool = False):
+        if console:
+            print(f'[Error] {message}; {exception}')
+        self.logger.exception(message, exception)
+
+    def info(self, message: str, console: bool = False):
         if console:
             print(f'[Info] {message}')
         self.logger.info(message)
 
-    def debug(self, message, console=False):
+    def debug(self, message: str, console: bool = False):
         if console:
             print(f'[Debug] {message}')
         self.logger.debug(message)
